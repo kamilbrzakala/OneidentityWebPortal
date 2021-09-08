c004466f92277f43370e02d2ea07a4c680e311a2
Kamil <brzakalakamil@gmail.com>
2021-09-08 23:13:12 +0200

    Adding default route config

--- WebPortal.WebUI/App_Start/RouteConfig.cs
+++ WebPortal.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace WebPortal.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Strona{page}",
+                defaults: new {Controller = "Person", action = "List"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Person", action = "List", id = UrlParameter.Optional }
